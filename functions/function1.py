d3d33093f46f02d75b8ae19e4be5218da0929851
samirvithlani <samir.vithlani83955@gmail.com>
2023-01-18 13:44:40 +0530

    function

---
+++ functions/function1.py
@@ -0,0 +1,43 @@
+#function :-function is a block of code which only runs when it is called.
+#function is a collection of statements which perform a specific task.
+#type of function :-1) built-in function 2) user defined function
+#WITH RETURN TYPE WITH PARAMETER
+#WITH RETURN TYPE WITHOUT PARAMETER
+#WITHOUT RETURN TYPE WITH PARAMETER
+#WITHOUT RETURN TYPE WITHOUT PARAMETER
+#def
+#use:-1) code reusability 2) code modularity 3) code readability
+#boilerplate problem....
+
+def add():
+    #without return type without parameter
+    print("hello")
+
+def sum(a,b):
+    #c = a + b
+    #return a + b
+    return  100
+
+
+def div():
+    return 15
+    return 20
+
+add()
+x = sum("raj",12)
+print(x)  
+
+x1 = div()
+print("div",x1)
+
+
+def display():
+    pass
+
+def demo(y):
+    print(",m,,,,,",z)
+    yield 10 *y #stop
+    yield 20 *y
+z=1
+for z in demo(z):
+    print(z)   
